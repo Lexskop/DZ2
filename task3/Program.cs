383fd47284ab053930522022c5bef4b121e598f0
lexskop <lexskop@gmail.com>
2022-09-20 02:50:12 +0700

    Добавил решение третьей задачи

--- task3/Program.cs
+++ task3/Program.cs
@@ -4,3 +4,30 @@
 7 -> да
 1 -> нет
 */
+
+Console.Clear();
+int error = 0;
+int userChoice = 1;
+while (error == 1 || userChoice == 1)
+{
+    error = 0;
+Console.WriteLine("Введите цифру дня недели: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+if (userNumber == 1 || userNumber == 2 || userNumber == 3 || userNumber == 4 || userNumber == 5)
+{
+    Console.WriteLine("Этот день не является выходным");
+    Console.WriteLine("Вы хотите продолжить работу с программой? Да - 1, Нет - 0");
+    userChoice = Convert.ToInt32(Console.ReadLine());
+}
+else if (userNumber == 6 || userNumber == 7)
+{
+    Console.WriteLine("Этот день является выходным");
+    Console.WriteLine("Вы хотите продолжить работу с программой? Да - 1, Нет - 0");
+    userChoice = Convert.ToInt32(Console.ReadLine());
+    }
+else
+{
+    Console.WriteLine("Вы ввели неккоректное число. Введите верное число");
+    error = 1;
+}
+}
