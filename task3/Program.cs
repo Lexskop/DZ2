062d0068b3f22801c64cacbbada3e46f4a446c72
lexskop <lexskop@gmail.com>
2022-09-20 11:20:48 +0700

    Нарастил код для третьей задачки

--- task3/Program.cs
+++ task3/Program.cs
@@ -6,28 +6,69 @@
 */
 
 Console.Clear();
+Console.WriteLine("Приветствую! Эта программа покажет, является ли введенный вами день недели - выходным!");
 int error = 0;
-int userChoice = 1;
-while (error == 1 || userChoice == 1)
+string userChoice = "y";
+while (error == 1 || userChoice.ToLower() == "y")
 {
     error = 0;
-Console.WriteLine("Введите цифру дня недели: ");
-int userNumber = Convert.ToInt32(Console.ReadLine());
-if (userNumber == 1 || userNumber == 2 || userNumber == 3 || userNumber == 4 || userNumber == 5)
-{
-    Console.WriteLine("Этот день не является выходным");
-    Console.WriteLine("Вы хотите продолжить работу с программой? Да - 1, Нет - 0");
-    userChoice = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите цифру дня недели: ");
+    int userNumber = Convert.ToInt32(Console.ReadLine());
+    if (userNumber == 1 || userNumber == 2 || userNumber == 3 || userNumber == 4 || userNumber == 5)
+        {
+            if (userNumber == 1)
+            {
+                Console.Write("Вы выбрали Понедельник");
+            }
+                else if (userNumber == 2)
+                {
+                    Console.Write("Вы выбрали Вторник");
+                }
+                    else if (userNumber == 3)
+                    {
+                        Console.Write("Вы выбрали Среду");
+                    }
+                        else if (userNumber == 4)
+                        {
+                            Console.Write("Вы выбрали Четверг");
+                        }
+                            else if (userNumber == 5)
+                            {
+                                Console.Write("Вы выбрали Пятницу");
+                            }
+            Console.WriteLine(" -> Этот день не является выходным");
+            
+            Console.WriteLine("Вы хотите продолжить работу с программой? Да - Y, Нет - N");
+            userChoice = Console.ReadLine();
+                while (userChoice.ToLower() != "y" && userChoice.ToLower() != "n")
+                {
+                    Console.WriteLine("Пожалуйста, введите верное решение. Если хотите продолжить работу - введите Y, если желаете закрыть программу - введите N");
+                    userChoice = Console.ReadLine();
+                }
+        }
+    else if (userNumber == 6 || userNumber == 7)
+            {
+                if (userNumber == 6)
+                {
+                    Console.Write("Вы выбрали Субботу");
+                }
+                else if (userNumber == 7)
+                    {
+                        Console.Write("Вы выбрали Воскресенье");
+                    }
+                Console.WriteLine(" -> Этот день является выходным");
+                Console.WriteLine("Вы хотите продолжить работу с программой? Да - Y, Нет - N");
+                userChoice = Console.ReadLine();
+                while (userChoice.ToLower() != "y" && userChoice.ToLower() != "n")
+                {
+                    Console.WriteLine("Пожалуйста, введите верное решение. Если хотите продолжить работу - введите Y, если желаете закрыть программу - введите N");
+                    userChoice = Console.ReadLine();
+                }
+            }
+        else
+            {
+                Console.WriteLine("Вы ввели неккоректное число. Введите верное число");
+                error = 1;
+            }
 }
-else if (userNumber == 6 || userNumber == 7)
-{
-    Console.WriteLine("Этот день является выходным");
-    Console.WriteLine("Вы хотите продолжить работу с программой? Да - 1, Нет - 0");
-    userChoice = Convert.ToInt32(Console.ReadLine());
-    }
-else
-{
-    Console.WriteLine("Вы ввели неккоректное число. Введите верное число");
-    error = 1;
-}
-}
+Console.WriteLine("Bye!");
